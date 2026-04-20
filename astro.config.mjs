b3a95b3b2f79065afc1eafcbbe35af729c9c3b3f
Nathan Constance | Rhys Welsh Ltd <nathan@rhyswelsh.com>
2026-04-20 20:18:43 +0100

    chore: configure Astro integrations

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,24 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
+import react from '@astrojs/react';
+import markdoc from '@astrojs/markdoc';
+import sitemap from '@astrojs/sitemap';
+import tailwind from '@astrojs/tailwind';
+import cloudflare from '@astrojs/cloudflare';
+import keystatic from '@keystatic/astro';
 
-// https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: 'https://nc-digital.co.uk',
+  output: 'static',
+  adapter: cloudflare(),
+  integrations: [
+    react(),
+    markdoc(),
+    sitemap(),
+    tailwind({ applyBaseStyles: false }),
+    ...(process.env.NODE_ENV !== 'production' ? [keystatic()] : []),
+  ],
+  image: {
+    service: { entrypoint: 'astro/assets/services/sharp' },
+  },
+});
